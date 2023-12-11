fa4e2cedcaf42cdfba3ef07784b2d36145e0ccf8
Júnior Gouveia <77864380+goudev@users.noreply.github.com>
2023-12-11 08:17:50 -0300

    Update IR_DEBUG.ino

--- IR/IR_DEBUG.ino
+++ IR/IR_DEBUG.ino
@@ -1,44 +1,71 @@
 #include <Arduino.h>
-#include <IRremoteESP8266.h>
+#include <assert.h>
 #include <IRrecv.h>
+#include <IRremoteESP8266.h>
+#include <IRac.h>
+#include <IRtext.h>
 #include <IRutils.h>
 
-// Defina o pino que o receptor IR está conectado
+// ==================== start of TUNEABLE PARAMETERS ====================
+// An IR detector/demodulator is connected to GPIO pin 14
+// e.g. D5 on a NodeMCU board.
+// Note: GPIO 16 won't work on the ESP8266 as it does not have interrupts.
+// Note: GPIO 14 won't work on the ESP32-C3 as it causes the board to reboot.
+#ifdef ARDUINO_ESP32C3_DEV
+const uint16_t kRecvPin = 10;  // 14 on a ESP32-C3 causes a boot loop.
+#else  // ARDUINO_ESP32C3_DEV
 const uint16_t kRecvPin = 23;
+#endif  // ARDUINO_ESP32C3_DEV
 
-// Define o tamanho do buffer de captura. Ajuste conforme necessário.
+const uint32_t kBaudRate = 115200;
 const uint16_t kCaptureBufferSize = 1024;
 
-// Cria uma instância do receptor IR
-IRrecv irrecv(kRecvPin, kCaptureBufferSize, 15, true);
+#if DECODE_AC
+#else   // DECODE_AC
+const uint8_t kTimeout = 15;
 
-// Estrutura para armazenar os resultados da decodificação
-decode_results results;
+#endif  // DECODE_AC
+const uint16_t kMinUnknownSize = 12;
+const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
+#define LEGACY_TIMING_INFO false
+IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeoutMs, true);
+decode_results results;  // Somewhere to store the results
 
 void setup() {
-  Serial.begin(115200);
-  irrecv.enableIRIn();  // Inicia o receptor
-  Serial.println("Pronto para receber sinais IR...");
+#if defined(ESP8266)
+  Serial.begin(kBaudRate, SERIAL_8N1, SERIAL_TX_ONLY);
+#else  // ESP8266
+  Serial.begin(kBaudRate, SERIAL_8N1);
+#endif  // ESP8266
+  while (!Serial)  // Wait for the serial connection to be establised.
+    delay(50);
+  assert(irutils::lowLevelSanityCheck() == 0);
+  Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", kRecvPin);
+#if DECODE_HASH
+  irrecv.setUnknownThreshold(kMinUnknownSize);
+#endif  // DECODE_HASH
+  irrecv.setTolerance(kTolerancePercentage);  // Override the default tolerance.
+  irrecv.enableIRIn();  // Start the receiver
 }
-
 void loop() {
   if (irrecv.decode(&results)) {
-    // Exibe o código IR recebido
-    Serial.print("Código IR Recebido: 0x");
-    Serial.println(results.value, HEX);
-
-    // Exibe o número de bits do código IR
-    Serial.print("Bits: ");
-    Serial.println(results.bits);
-
-    // Exibe o tipo de protocolo
-    if (results.decode_type == UNKNOWN) {
-      Serial.println("Protocolo desconhecido");
-    } else {
-      Serial.print("Protocolo: ");
-      Serial.println(typeToString(results.decode_type));
-    }
-
-    irrecv.resume();  // Prepara para receber o próximo valor
+    uint32_t now = millis();
+    Serial.printf(D_STR_TIMESTAMP " : %06u.%03u\n", now / 1000, now % 1000);
+    if (results.overflow)
+      Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
+    Serial.println(D_STR_LIBRARY "   : v" _IRREMOTEESP8266_VERSION_STR "\n");
+    if (kTolerancePercentage != kTolerance)
+      Serial.printf(D_STR_TOLERANCE " : %d%%\n", kTolerancePercentage);
+    Serial.print(resultToHumanReadableBasic(&results));
+    String description = IRAcUtils::resultAcToString(&results);
+    if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
+    yield();  // Feed the WDT as the text output can take a while to print.
+#if LEGACY_TIMING_INFO
+    Serial.println(resultToTimingInfo(&results));
+    yield();  // Feed the WDT (again)
+#endif  // LEGACY_TIMING_INFO
+    Serial.println(resultToSourceCode(&results));
+    Serial.println();    // Blank line between entries
+    yield();             // Feed the WDT (again)
   }
 }
